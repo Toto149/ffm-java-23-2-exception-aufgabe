42f4c26dd722ecd041862b157e51b1573c4471f6
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-17 17:02:50 +0100

    add tests and functions GuestList via TDD

--- src/main/java/GuestList.java
+++ src/main/java/GuestList.java
@@ -1,15 +1,37 @@
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-@Data
+
 @Builder
+@ToString
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class GuestList {
     private List<String> guests = new ArrayList<>();
 
+    public void setGuests(List<String> guests) throws IOException {
+        this.guests = guests;
+        Files.write(Path.of("C:\\Users\\Thorsten Thomann" +
+                "\\IdeaProjects\\ffm-java-23-2-exception-aufgabe" +
+                "\\src\\main\\java\\guests.txt"),guests);
+    }
+    public List<String> getGuests() {
+        try {
+            if(Files.exists(Path.of("C:\\Users\\Thorsten Thomann" +
+                    "\\IdeaProjects\\ffm-java-23-2-exception-aufgabe" +
+                    "\\src\\main\\java\\guests.txt"))){
+                return Files.readAllLines(Path.of("C:\\Users\\Thorsten Thomann" +
+                        "\\IdeaProjects\\ffm-java-23-2-exception-aufgabe" +
+                        "\\src\\main\\java.guests.txt"));
+            }
+        } catch (IOException e){
+            System.out.println("guests.txt does not exist");
+        }
+        return this.guests;
+    }
 }
--- src/test/java/GuestListTest.java
+++ src/test/java/GuestListTest.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class GuestListTest {
 
     @Test
-    void shouldBeEmptyInitially(){
+    void shouldBeEmptyInitially() throws IOException {
         //GIVEN
         GuestList guestList = new GuestList();
         List<String> expected = List.of();
@@ -17,7 +21,7 @@ public class GuestListTest {
         assertLinesMatch(expected,actual);
     }
     @Test
-    void shouldReadSameGuestsAsWrittenBefore(){
+    void shouldReadSameGuestsAsWrittenBefore() throws IOException {
         //GIVEN
         GuestList guestList = new GuestList();
         List<String> expected = List.of("Karl","Ute");
@@ -26,5 +30,33 @@ public class GuestListTest {
         List<String> actual = guestList.getGuests();
         //THEN
         assertLinesMatch(expected,actual);
+    }
+    @Test
+    void shouldWriteToFileSystem() throws IOException {
+        //GIVEN
+        GuestList guestList = new GuestList();
+        List<String> input = List.of("Theodor", "Anette");
+        guestList.setGuests(input);
+        Path expected = Path.of("C:\\Users\\Thorsten Thomann" +
+                "\\IdeaProjects\\ffm-java-23-2-exception-aufgabe" +
+                "\\src\\main\\java\\guests.txt");
+        //WHEN
+        Boolean actual = Files.exists(expected);
+
+        //THEN
+        assertTrue(actual);
+    }
+    @Test
+    void shouldReadFromFileSystem() throws IOException {
+        //GIVEN
+        List<String> expected = Files.readAllLines(Path.of("C:\\Users\\Thorsten Thomann" +
+                "\\IdeaProjects\\ffm-java-23-2-exception-aufgabe" +
+                "\\src\\main\\java\\guests.txt"));
+        GuestList guestList = new GuestList();
+        //WHEN
+        List<String> actual = guestList.getGuests();
+        //THEN
+        assertLinesMatch(expected,actual);
+
     }
 }
